688e66cf883733f1d8063706fe06e0f4601c3042
Ismatullo Mukhamedzhanov <ismatullo.mukhamedzhanov_2022@UCA.corp>
2021-09-10 18:20:15 +0500

    first commit

---
+++ HW13/HW13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Cat {
+        int id;
+        string Name;
+        string Color;
+    }
+}
